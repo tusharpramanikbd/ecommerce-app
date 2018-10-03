29b0f07be630dea4ac9b9bb5fd60ae3e0a13ad86
tuse1993 <tuse1993@gmail.com>
2018-10-03 17:39:15 +0600

    fixed different screen size problem for login view,signup ui implemented...

--- app/src/main/java/com/nitto/tushar/nrrii/Activity/LoginActivity.java
+++ app/src/main/java/com/nitto/tushar/nrrii/Activity/LoginActivity.java
@@ -1,15 +1,33 @@
 package com.nitto.tushar.nrrii.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
 
 import com.nitto.tushar.nrrii.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private AppCompatButton btnRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initializeUI();
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, SignUpActivity.class) );
+            }
+        });
+    }
+
+    private void initializeUI() {
+        btnRegister = findViewById(R.id.btnRegister);
     }
 }
---
+++ app/src/main/java/com/nitto/tushar/nrrii/Activity/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.nitto.tushar.nrrii.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nitto.tushar.nrrii.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
