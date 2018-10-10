454c965af2e3608a403eb669b33eb062c61411bc
tuse1993 <tuse1993@gmail.com>
2018-10-10 16:28:45 +0600

    ProductDetailsActivity UI fully implemented...

--- app/src/main/java/com/nitto/tushar/nrrii/Activity/ProductDetailsActivity.java
+++ app/src/main/java/com/nitto/tushar/nrrii/Activity/ProductDetailsActivity.java
@@ -1,17 +1,36 @@
 package com.nitto.tushar.nrrii.Activity;
 
+import android.content.Context;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.AppCompatImageView;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.nitto.tushar.nrrii.Adapter.MyPagerAdapter;
+import com.nitto.tushar.nrrii.Adapter.RecyclerViewAdapterDress;
+import com.nitto.tushar.nrrii.Entity.Dress;
 import com.nitto.tushar.nrrii.R;
+
+import java.util.ArrayList;
 
 public class ProductDetailsActivity extends AppCompatActivity {
 
     private ViewPager mPeger;
     private MyPagerAdapter myPagerAdapter;
+    private ArrayList<Dress> dressArrayList;
+    private AppCompatButton btnReadMore;
+
+    private LayoutInflater linf;
+    private LinearLayout rr;
+
+    private LinearLayout layoutReview, reviewItem;
+
     private int[] images = {R.mipmap.rifat_vais_wife_sample, R.mipmap.rifat_vais_wife_sample, R.mipmap.rifat_vais_wife_sample};
 
     private AppCompatImageView
@@ -51,6 +70,45 @@ public class ProductDetailsActivity extends AppCompatActivity {
 
             }
         });
+
+        btnReadMore = findViewById(R.id.btnReadMore);
+
+        btnReadMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                linf = (LayoutInflater) getApplicationContext().getSystemService(
+                        Context.LAYOUT_INFLATER_SERVICE);
+                linf = LayoutInflater.from(ProductDetailsActivity.this);
+
+                rr = (LinearLayout) findViewById(R.id.layoutReview);
+
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+                layoutParams.setMargins(0, 0, 0, 30);
+
+                for (int i = 1; i < 4; i++) {
+                    final View v1 = linf.inflate(R.layout.review_item, null);
+                    rr.addView(v1, layoutParams);
+                }
+            }
+        });
+
+        dressArrayList = new ArrayList<>();
+        dressArrayList.add(new Dress("5000", "4000"));
+        dressArrayList.add(new Dress("6000", "5000"));
+        dressArrayList.add(new Dress("7000", "5000"));
+        dressArrayList.add(new Dress("8000", "4000"));
+        dressArrayList.add(new Dress("9000", "4000"));
+        dressArrayList.add(new Dress("9000", "8000"));
+        dressArrayList.add(new Dress("8000", "7000"));
+        dressArrayList.add(new Dress("7000", "7000"));
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerViewDress);
+        RecyclerViewAdapterDress myAdapter = new RecyclerViewAdapterDress(this,dressArrayList);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setNestedScrollingEnabled(false);
     }
 
     private void resetWalkthroughTabSelectionColor() {
